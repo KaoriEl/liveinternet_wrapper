d5eaaa83df5e144d2bf545f5bade0c84158681d1
Rostislav Krivets <krivets.rostislav@yandex.ru>
2021-09-14 14:11:42 +0700

    Новый чекер прокси

--- app/Console/Commands/CheckProxy.php
+++ app/Console/Commands/CheckProxy.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use mysql_xdevapi\Exception;
+use GuzzleHttp\RequestOptions;
 
 class CheckProxy extends Command
 {
@@ -36,26 +38,52 @@ class CheckProxy extends Command
      * Чекает прокси на активность
      *
      * @return int
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function handle()
     {
         $proxies = DB::table("proxies")->get();
 
-        echo $proxies;
-
+        $arr = array();
         foreach ($proxies as $proxy){
 
-            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $connection =  @socket_connect($socket, $proxy->proxy_address, $proxy->proxy_port);
+            array_push($arr, $proxy->proxy_address . ":" . $proxy->proxy_port);
 
-            if( $connection ){
-                DB::table("proxies")->where("proxy_address", $proxy->proxy_address)->update(['status'=>"ACTIVE",]);
-                echo 'ONLINE';
+
+
+//            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+//            $connection =  @socket_connect($socket, $proxy->proxy_address, $proxy->proxy_port);
+//
+//            if( $connection ){
+//                DB::table("proxies")->where("proxy_address", $proxy->proxy_address)->update(['status'=>"ACTIVE",]);
+//                echo 'ONLINE';
+//            }
+//            else {
+//                DB::table("proxies")->where("proxy_address", $proxy->proxy_address)->update(['status'=>"INACTIVE",]);
+//                echo 'OFFLINE: ' . socket_strerror(socket_last_error( $socket ));
+//            }
+        }
+        $string = implode(PHP_EOL, $arr);
+
+        $http = new Client;
+        $response = $http->post('https://proxy-checker.net/api/proxy-checker/', [
+            'form_params' => [
+                'proxy_list' => $string,
+            ],
+        ]);
+
+        $proxies = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($proxies as $proxy){
+            if($proxy["valid"] == "true"){
+                DB::table("proxies")->where("proxy_address", $proxy["ip"])->update(['status'=>"ACTIVE",]);
+                echo 'ONLINE: ' . $proxy["ip"] . ":" . $proxy["port"] . PHP_EOL;
             }
             else {
-                DB::table("proxies")->where("proxy_address", $proxy->proxy_address)->update(['status'=>"INACTIVE",]);
-                echo 'OFFLINE: ' . socket_strerror(socket_last_error( $socket ));
+                DB::table("proxies")->where("proxy_address", $proxy["ip"])->update(['status'=>"INACTIVE",]);
+                echo 'OFFLINE: ' . $proxy["ip"] . ":" . $proxy["port"] . PHP_EOL;
             }
         }
+
     }
 }
